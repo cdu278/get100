7ed22813b973b840c40182803b9642576673aa34
cdu <cdu145@protonmail.com>
2021-06-18 15:30:42 +0300

    Init

---
+++ app/src/main/java/cdu145/tickets/MainActivity.kt
@@ -0,0 +1,12 @@
+package cdu145.tickets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
