32228d1f565bf9a952f941f7f6810181b33844cf
cdu <cdu145@protonmail.com>
2021-08-28 15:48:53 +0300

    Migrate test SolutionSigns() from vararg to 5 params

--- domain/src/test/java/tickets/solution/signs/SolutionSigns.kt
+++ domain/src/test/java/tickets/solution/signs/SolutionSigns.kt
@@ -1,6 +1,12 @@
 package tickets.solution.signs
 
 @Suppress("TestFunctionName")
-internal fun SolutionSigns(vararg signs: SolutionSign): SolutionSigns {
-    return ListSolutionSigns(signs.toList())
+internal fun SolutionSigns(
+    s1: SolutionSign,
+    s2: SolutionSign,
+    s3: SolutionSign,
+    s4: SolutionSign,
+    s5: SolutionSign,
+): SolutionSigns {
+    return ListSolutionSigns(listOf(s1, s2, s3, s4, s5))
 }
