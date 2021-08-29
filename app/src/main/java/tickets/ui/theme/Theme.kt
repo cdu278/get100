ef987c862d1cf5716b8e5e690a8648bde64fdb2e
cdu <cdu145@protonmail.com>
2021-08-29 22:30:07 +0300

    Set up Compose

---
+++ app/src/main/java/tickets/ui/theme/Theme.kt
@@ -0,0 +1,21 @@
+package tickets.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ComposeTicketsTheme(
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colors = lightColors(
+            primary = Purple500,
+            primaryVariant = Purple700,
+            secondary = Teal200,
+        ),
+        typography = Typography,
+        shapes = Shapes,
+        content,
+    )
+}
