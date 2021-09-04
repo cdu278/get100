5bb2c08f6ae4f92f4b14e206cac89347db052004
cdu <cdu145@protonmail.com>
2021-09-04 18:14:41 +0300

    Persistent: add Mutable subtype interface

--- domain/src/main/java/tickets/persistent/Persistent.kt
+++ domain/src/main/java/tickets/persistent/Persistent.kt
@@ -3,4 +3,9 @@ package tickets.persistent
 interface Persistent<out T> {
 
     suspend fun actual(): T
+
+    interface Mutable<T> : Persistent<T> {
+
+        suspend fun mutate(newValue: T)
+    }
 }
