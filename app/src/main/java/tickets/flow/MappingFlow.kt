d91acf0dead3b18791809a081554d126bc4671b8
cdu <cdu145@protonmail.com>
2021-09-06 00:31:46 +0300

    Add MappingFlow fun

---
+++ app/src/main/java/tickets/flow/MappingFlow.kt
@@ -0,0 +1,12 @@
+package tickets.flow
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+@Suppress("FunctionName")
+fun <T, R> MappingFlow(
+    flow: Flow<T>,
+    transform: suspend (T) -> R,
+): Flow<R> {
+    return flow.map(transform)
+}
