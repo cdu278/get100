e846b2b11418baf93c47a3cceb68818d79f1e27b
cdu <cdu145@protonmail.com>
2021-09-06 23:02:32 +0300

    SolutionSigns: add Empty val

--- domain/src/main/java/tickets/solution/signs/SolutionSigns.kt
+++ domain/src/main/java/tickets/solution/signs/SolutionSigns.kt
@@ -5,4 +5,12 @@ import tickets.solution.signs.position.SignPosition
 interface SolutionSigns {
 
     operator fun get(position: SignPosition): SolutionSign
+
+    companion object {
+
+        val Empty: SolutionSigns = object : SolutionSigns {
+
+            override fun get(position: SignPosition): SolutionSign = ArithmeticSign.NONE
+        }
+    }
 }
