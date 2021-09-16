7b7d1bc48383e2df0759c255872ec826e2734791
cdu <cdu145@protonmail.com>
2021-09-16 21:48:43 +0300

    Add notEquivalentTo() for TicketDigits

--- domain/src/main/java/tickets/digits/EquivalentTo.kt
+++ domain/src/main/java/tickets/digits/EquivalentTo.kt
@@ -6,3 +6,10 @@ infix fun TicketDigits.equivalentTo(other: TicketDigits): Boolean {
     }
     return true
 }
+
+infix fun TicketDigits.notEquivalentTo(other: TicketDigits): Boolean {
+    repeat(6) { i ->
+        if (this[i].value != other[i].value) return true
+    }
+    return false
+}
