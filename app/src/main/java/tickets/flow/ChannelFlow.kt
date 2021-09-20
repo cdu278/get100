f1c3d2577ab0b974a5f11604b9eaa1d1ad717a66
cdu <cdu145@protonmail.com>
2021-09-20 21:39:12 +0300

    Add ChannelFlow fun

---
+++ app/src/main/java/tickets/flow/ChannelFlow.kt
@@ -0,0 +1,8 @@
+package tickets.flow
+
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+@Suppress("FunctionName")
+fun <T> ChannelFlow(channel: ReceiveChannel<T>): Flow<T> = channel.receiveAsFlow()
