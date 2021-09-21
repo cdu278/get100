cdb999ffc826e4c86708a038b51cb4bcc6524d98
cdu <cdu145@protonmail.com>
2021-09-21 15:36:48 +0300

    Set default available hints count to 100

--- app/src/main/java/tickets/hint/available/AvailableHintCountDataStore.kt
+++ app/src/main/java/tickets/hint/available/AvailableHintCountDataStore.kt
@@ -8,5 +8,5 @@ import tickets.datastore.serializer.IntSerializer
 val Context.availableHintCountDataStore: DataStore<Int>
         by dataStore(
             fileName = "available_hint_count.bin",
-            serializer = IntSerializer(defaultValue = 20),
+            serializer = IntSerializer(defaultValue = 100),
         )
