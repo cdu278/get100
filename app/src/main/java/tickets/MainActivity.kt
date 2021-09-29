5ac105f82cdae6021ec2928add9010f5783e53c8
cdu <cdu145@protonmail.com>
2021-09-29 21:41:04 +0300

    themes.xml: remove `background` item

--- app/src/main/java/tickets/MainActivity.kt
+++ app/src/main/java/tickets/MainActivity.kt
@@ -3,10 +3,12 @@ package tickets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,7 +30,8 @@ private fun Content() {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(),
+                .fillMaxHeight()
+                .background(MaterialTheme.colors.background),
         ) {
             TicketsTopBar()
 
