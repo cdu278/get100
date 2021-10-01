b1fc507fd8b8a066f98ebdbb5d2c7dcff8b9d1d6
cdu <cdu145@protonmail.com>
2021-10-01 16:26:38 +0300

    ClearSolutionButton: add icon

--- app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
+++ app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
@@ -1,19 +1,28 @@
 package tickets.solution.clear
 
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import cdu145.tickets.R
 import org.koin.androidx.compose.getViewModel
-import tickets.ui.BottomButton
+import tickets.ui.Margin
 
 @Composable
 fun ClearSolutionButton(
     viewModel: ClearSolutionButtonViewModel = getViewModel<ClearSolutionButtonViewModelImpl>(),
 ) {
-    BottomButton(
+    Button(
         onClick = { viewModel.clearSolution() },
     ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_clear),
+            contentDescription = null,
+        )
+        Margin(4.dp)
         Text(stringResource(R.string.clearSolutionButton_text))
     }
 }
