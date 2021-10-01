b8dcaa0cd820382e64897a8eb4e8fdfc56e55bce
cdu <cdu145@protonmail.com>
2021-10-01 16:26:24 +0300

    NextNumberButton: add icon

--- app/src/main/java/tickets/digits/next/NextNumberButton.kt
+++ app/src/main/java/tickets/digits/next/NextNumberButton.kt
@@ -1,19 +1,28 @@
 package tickets.digits.next
 
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import cdu145.tickets.R
 import org.koin.androidx.compose.getViewModel
-import tickets.ui.BottomButton
+import tickets.ui.Margin
 
 @Composable
 fun NextNumberButton(
     viewModel: NextNumberButtonViewModel = getViewModel<NextNumberButtonViewModelImpl>(),
 ) {
-    BottomButton(
+    Button(
         onClick = { viewModel.loadNextNumber() },
     ) {
         Text(text = stringResource(R.string.nextNumberButton_text))
+        Margin(4.dp)
+        Icon(
+            painter = painterResource(R.drawable.ic_next),
+            contentDescription = null,
+        )
     }
 }
