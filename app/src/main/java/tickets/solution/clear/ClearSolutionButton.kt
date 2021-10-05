5722b78007be562e9af07874377e9195eb0fb2c6
cdu <cdu145@protonmail.com>
2021-10-05 14:34:31 +0300

    ClearSolutionButton: implement hiding when ticket solved

--- app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
+++ app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
@@ -1,9 +1,14 @@
 package tickets.solution.clear
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -15,8 +20,11 @@ import tickets.ui.Margin
 fun ClearSolutionButton(
     viewModel: ClearSolutionButtonViewModel = getViewModel(),
 ) {
+    val shown by viewModel.shown.collectAsState()
+    val shownRatio by animateFloatAsState(targetValue = if (shown) 1f else 0f)
     Button(
         onClick = { viewModel.clearSolution() },
+        modifier = Modifier.graphicsLayer(alpha = shownRatio),
     ) {
         Icon(
             painter = painterResource(R.drawable.ic_clear),
