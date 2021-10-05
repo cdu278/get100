f8187ed9ea7daa335a6ff6889144c88c552a9ac9
cdu <cdu145@protonmail.com>
2021-10-05 19:12:55 +0300

    Typography: remove overridings

--- app/src/main/java/tickets/ui/theme/Type.kt
+++ app/src/main/java/tickets/ui/theme/Type.kt
@@ -1,11 +1,9 @@
 package tickets.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import cdu145.tickets.R
 
 val Typography = Typography(
@@ -13,21 +11,4 @@ val Typography = Typography(
         Font(R.font.rubik_regular, weight = FontWeight.Normal),
         Font(R.font.rubik_medium, weight = FontWeight.Medium),
     ),
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
