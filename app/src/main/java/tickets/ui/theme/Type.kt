689604d9781fe8df1dcb8c27457ae3404a84e03b
cdu <cdu145@protonmail.com>
2021-10-05 19:09:03 +0300

    Change default font family

--- app/src/main/java/tickets/ui/theme/Type.kt
+++ app/src/main/java/tickets/ui/theme/Type.kt
@@ -2,11 +2,17 @@ package tickets.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import cdu145.tickets.R
 
 val Typography = Typography(
+    defaultFontFamily = FontFamily(
+        Font(R.font.rubik_regular, weight = FontWeight.Normal),
+        Font(R.font.rubik_medium, weight = FontWeight.Medium),
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
