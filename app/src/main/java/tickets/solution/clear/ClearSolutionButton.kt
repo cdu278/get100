876b8adc7973b8ab8813f67d13f95cc44bcdbc81
cdu <cdu145@protonmail.com>
2021-10-15 18:42:04 +0300

    ClearSolutionButton: migrate to using BottomButton

--- app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
+++ app/src/main/java/tickets/solution/clear/ClearSolutionButton.kt
@@ -1,7 +1,6 @@
 package tickets.solution.clear
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,10 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import cdu145.tickets.R
 import org.koin.androidx.compose.getViewModel
-import tickets.ui.Margin
+import tickets.ui.BottomButton
 
 @Composable
 fun ClearSolutionButton(
@@ -22,15 +20,20 @@ fun ClearSolutionButton(
 ) {
     val shown by viewModel.shown.collectAsState()
     val shownRatio by animateFloatAsState(targetValue = if (shown) 1f else 0f)
-    Button(
+    BottomButton(
         onClick = { viewModel.clearSolution() },
+        iconGravity = BottomButton.IconGravity.Left,
         modifier = Modifier.graphicsLayer(alpha = shownRatio),
-    ) {
-        Icon(
-            painter = painterResource(R.drawable.ic_clear),
-            contentDescription = null,
-        )
-        Margin(4.dp)
-        Text(stringResource(R.string.clearSolutionButton_text))
-    }
+        icon = {
+            Icon(
+                painter = painterResource(R.drawable.ic_clear),
+                contentDescription = null,
+            )
+        },
+        text = {
+            Text(
+                stringResource(R.string.clearSolutionButton_text),
+            )
+        },
+    )
 }
