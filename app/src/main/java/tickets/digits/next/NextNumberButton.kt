a9940433e2e5b1caddfc902d3a719eb4344b83cd
cdu <cdu145@protonmail.com>
2021-10-15 18:41:52 +0300

    NextNumberButton: migrate to using BottomButton

--- app/src/main/java/tickets/digits/next/NextNumberButton.kt
+++ app/src/main/java/tickets/digits/next/NextNumberButton.kt
@@ -1,28 +1,33 @@
 package tickets.digits.next
 
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import cdu145.tickets.R
 import org.koin.androidx.compose.getViewModel
-import tickets.ui.Margin
+import tickets.ui.BottomButton
 
 @Composable
 fun NextNumberButton(
     viewModel: NextNumberButtonViewModel = getViewModel<NextNumberButtonViewModelImpl>(),
 ) {
-    Button(
+    BottomButton(
         onClick = { viewModel.loadNextNumber() },
-    ) {
-        Text(text = stringResource(R.string.nextNumberButton_text))
-        Margin(4.dp)
-        Icon(
-            painter = painterResource(R.drawable.ic_next),
-            contentDescription = null,
-        )
-    }
+        iconGravity = BottomButton.IconGravity.Right,
+        modifier = Modifier, // Workaround for the NPE bug
+        icon = {
+            Icon(
+                painter = painterResource(R.drawable.ic_next),
+                contentDescription = null,
+            )
+        },
+        text = {
+            Text(
+                stringResource(R.string.nextNumberButton_text),
+            )
+        },
+    )
 }
