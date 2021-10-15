d25fe4081129acdf925280a6a1073291b488a9a8
cdu <cdu145@protonmail.com>
2021-10-15 18:41:18 +0300

    BottomButton: make language-depending
    Shows no text for Russian

--- app/src/main/java/tickets/ui/BottomButton.kt
+++ app/src/main/java/tickets/ui/BottomButton.kt
@@ -1,20 +1,53 @@
 package tickets.ui
 
-import androidx.compose.foundation.layout.RowScope
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
+import tickets.ui.BottomButton.IconGravity.Left
+import tickets.ui.BottomButton.IconGravity.Right
 
-@Composable
-fun BottomButton(
-    onClick: () -> Unit,
-    content: @Composable RowScope.() -> Unit,
-) {
-    Button(
-        onClick,
-        modifier = Modifier.width(96.dp),
-        content = content,
-    )
+object BottomButton {
+
+    enum class IconGravity { Right, Left }
+
+    @SuppressLint("ComposableNaming")
+    @Composable
+    operator fun invoke(
+        onClick: () -> Unit,
+        iconGravity: IconGravity,
+        modifier: Modifier = Modifier,
+        icon: @Composable () -> Unit,
+        text: @Composable () -> Unit,
+    ) {
+        Button(
+            onClick,
+            modifier = modifier.setWidthDependingOnLanguage(),
+        ) {
+            if (iconGravity == Left) {
+                icon()
+                Margin(4.dp)
+            }
+            if (Locale.current.language != "ru") {
+                text()
+            }
+            if (iconGravity == Right) {
+                Margin(4.dp)
+                icon()
+            }
+        }
+    }
+
+    private fun Modifier.setWidthDependingOnLanguage(): Modifier {
+        return then(
+            if (Locale.current.language == "ru") {
+                Modifier.width(100.dp)
+            } else {
+                Modifier
+            }
+        )
+    }
 }
