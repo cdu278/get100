0de7d0e4ace1c440911ab45a505a7353195ea199
cdu <cdu145@protonmail.com>
2021-10-19 01:16:52 +0300

    Remove interface of RemainingRestorationTime

--- app/src/main/java/cdu145/tickets/hint/restoring/RemainingRestorationTime.kt
+++ app/src/main/java/cdu145/tickets/hint/restoring/RemainingRestorationTime.kt
@@ -2,34 +2,23 @@ package cdu145.tickets.hint.restoring
 
 import kotlinx.coroutines.delay
 
-interface RemainingRestorationTime {
+class RemainingRestorationTime(
+    private val millis: Long,
+) {
 
     val seconds: Int
+        get() {
+            val seconds = (millis / 1000).toInt()
+            return if (millis % 1000 == 0L) seconds else seconds + 1
+        }
 
     val over: Boolean
+        get() = millis == 0L
 
-    suspend fun tick(): RemainingRestorationTime
-}
-
-fun RemainingRestorationTime(
-    millis: Long,
-): RemainingRestorationTime {
-    return object : RemainingRestorationTime {
-
-        override val seconds: Int
-            get() {
-                val seconds = (millis / 1000).toInt()
-                return if (millis % 1000 == 0L) seconds else seconds + 1
-            }
-
-        override val over: Boolean
-            get() = millis == 0L
-
-        override suspend fun tick(): RemainingRestorationTime {
-            require(!over) { "Restoration time is over" }
-            val delayTime = (millis % 1000).takeIf { it != 0L } ?: 1000L
-            delay(delayTime)
-            return RemainingRestorationTime(millis - delayTime)
-        }
+    suspend fun tick(): RemainingRestorationTime {
+        require(!over) { "Restoration time is over" }
+        val delayTime = (millis % 1000).takeIf { it != 0L } ?: 1000L
+        delay(delayTime)
+        return RemainingRestorationTime(millis - delayTime)
     }
 }
