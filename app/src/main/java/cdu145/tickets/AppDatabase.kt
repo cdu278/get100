5af602267a3801373bab0da005f468837b2d9d69
cdu <cdu145@protonmail.com>
2021-10-20 17:09:15 +0300

    AppDatabase: set exportSchema = false

--- app/src/main/java/cdu145/tickets/AppDatabase.kt
+++ app/src/main/java/cdu145/tickets/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import cdu145.tickets.hint.restoring.RestoringHintsDao
 import cdu145.tickets.hint.restoring.RestoringHint
 
-@Database(entities = [RestoringHint::class], version = 1)
+@Database(entities = [RestoringHint::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract val restoringHintsDao: RestoringHintsDao
