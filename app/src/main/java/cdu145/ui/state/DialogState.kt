11de169fed13e5f56e5f807bf006632161006d05
cdu <cdu145@protonmail.com>
2021-10-23 15:36:20 +0300

    DialogState: add Companion.shownIf()

--- app/src/main/java/cdu145/ui/state/DialogState.kt
+++ app/src/main/java/cdu145/ui/state/DialogState.kt
@@ -1,3 +1,13 @@
 package cdu145.ui.state
 
-enum class DialogState { Shown, Hidden }
+enum class DialogState {
+
+    Shown, Hidden;
+
+    companion object {
+
+        inline fun shownIf(condition: () -> Boolean): DialogState {
+            return if (condition()) Shown else Hidden
+        }
+    }
+}
