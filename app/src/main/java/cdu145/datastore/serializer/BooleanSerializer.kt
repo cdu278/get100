ba9c9e31741e00e2b3f3c03977fe2cd5dadc02ac
cdu <cdu145@protonmail.com>
2021-10-28 18:55:07 +0300

    Rewrite BooleanSerializer
    Implement Serializer by DataStreamSerializer

--- app/src/main/java/cdu145/datastore/serializer/BooleanSerializer.kt
+++ app/src/main/java/cdu145/datastore/serializer/BooleanSerializer.kt
@@ -1,31 +1,13 @@
 package cdu145.datastore.serializer
 
-import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
-import java.io.*
+import java.io.DataInputStream
+import java.io.DataOutputStream
 
 class BooleanSerializer(
     override val defaultValue: Boolean,
-    private val intSerializer: Serializer<Int> = IntSerializer(defaultValue.asInt()),
-) : Serializer<Boolean> {
-
-    override suspend fun readFrom(input: InputStream): Boolean {
-        return try {
-            intSerializer.readFrom(input).asBoolean()
-        } catch (e: CorruptionException) {
-            throw CorruptionException("Cannot read boolean value.", e)
-        }
-    }
-
-    override suspend fun writeTo(t: Boolean, output: OutputStream) {
-        try {
-            intSerializer.writeTo(t.asInt(), output)
-        } catch (e: CorruptionException) {
-            throw CorruptionException("Cannot write boolean value.", e)
-        }
-    }
-}
-
-private fun Boolean.asInt(): Int = if (this) 1 else 0
-
-private fun Int.asBoolean(): Boolean = this == 1
+) : Serializer<Boolean> by DataStreamSerializer(
+    defaultValue,
+    readValue = DataInputStream::readBoolean,
+    writeValue = DataOutputStream::writeBoolean,
+)
