7457a13631c0107b6957e8d6211cf7ea6e94201e
cdu <cdu145@protonmail.com>
2021-10-29 19:10:56 +0300

    Clean up DigitsOf.kt
    Remove unused annotation

--- domain/src/main/java/cdu145/tickets/digits/DigitsOf.kt
+++ domain/src/main/java/cdu145/tickets/digits/DigitsOf.kt
@@ -7,7 +7,6 @@ private fun TicketNumber.digitAt(position: Int): Int {
     return (this.value / 10.0.pow(5 - position).toInt()) % 10
 }
 
-@Suppress("FunctionName")
 fun digitsOf(ticketNumber: TicketNumber): TicketDigits {
     return ListTicketDigits(
         List(size = 6) { i -> ticketNumber.digitAt(i) },
