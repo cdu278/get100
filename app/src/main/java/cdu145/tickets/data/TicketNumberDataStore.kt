7139ee7153d8a3fcf0c078420d816f2d3ece5a98
cdu <cdu145@protonmail.com>
2021-12-09 14:31:01 +0300

    `ticketNumberDataStore`: inline serializer

--- app/src/main/java/cdu145/tickets/data/TicketNumberDataStore.kt
+++ app/src/main/java/cdu145/tickets/data/TicketNumberDataStore.kt
@@ -9,33 +9,31 @@ import cdu145.tickets.domain.number.TicketNumber
 import cdu145.tickets.domain.number.ZeroTicketNumber
 import java.io.*
 
+@Suppress("BlockingMethodInNonBlockingContext")
 val Context.ticketNumberDataStore: DataStore<TicketNumber>
         by dataStore(
             fileName = "ticket_number.bin",
-            serializer = TicketNumberSerializer,
+            serializer = object : Serializer<TicketNumber> {
+
+                override val defaultValue: TicketNumber
+                    get() = ZeroTicketNumber
+
+                override suspend fun readFrom(input: InputStream): TicketNumber {
+                    return try {
+                        TicketNumber(
+                            value = DataInputStream(input).readInt(),
+                        )
+                    } catch (e: IOException) {
+                        throw CorruptionException("Cannot read ticket number.", e)
+                    }
+                }
+
+                override suspend fun writeTo(t: TicketNumber, output: OutputStream) {
+                    try {
+                        DataOutputStream(output).writeInt(t.value)
+                    } catch (e: IOException) {
+                        throw CorruptionException("Cannot write ticket number.", e)
+                    }
+                }
+            }
         )
-
-@Suppress("BlockingMethodInNonBlockingContext")
-private object TicketNumberSerializer : Serializer<TicketNumber> {
-
-    override val defaultValue: TicketNumber
-        get() = ZeroTicketNumber
-
-    override suspend fun readFrom(input: InputStream): TicketNumber {
-        return try {
-            TicketNumber(
-                value = DataInputStream(input).readInt(),
-            )
-        } catch (e: IOException) {
-            throw CorruptionException("Cannot read ticket number.", e)
-        }
-    }
-
-    override suspend fun writeTo(t: TicketNumber, output: OutputStream) {
-        try {
-            DataOutputStream(output).writeInt(t.value)
-        } catch (e: IOException) {
-            throw CorruptionException("Cannot write ticket number.", e)
-        }
-    }
-}
