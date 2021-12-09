8c97bf07b3dfca8ab735a84b078681ba8c310d9e
cdu <cdu145@protonmail.com>
2021-12-09 14:18:54 +0300

    Remove unused `DialogState.Companion.shownIf()`

--- app/src/main/java/cdu145/model/DialogState.kt
+++ app/src/main/java/cdu145/model/DialogState.kt
@@ -1,13 +1,3 @@
 package cdu145.model
 
-enum class DialogState {
-
-    Shown, Hidden;
-
-    companion object {
-
-        inline fun shownIf(condition: () -> Boolean): DialogState {
-            return if (condition()) Shown else Hidden
-        }
-    }
-}
+enum class DialogState { Shown, Hidden }
